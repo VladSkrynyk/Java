100669502f9200aaf4ba6a0380cf3b4860f6606d
VladSkrynyk <108512047+VladSkrynyk@users.noreply.github.com>
2023-12-02 11:16:57 +0200

    Vlad Skrynyk 3 course compmat1 CW11 1.1-1.6

---
+++ task3.java
@@ -0,0 +1,11 @@
+package task3;
+
+import java.util.Scanner;
+
+public class task3 {
+    static public void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine().replace("small", "very large");
+        System.out.println(str);
+    }
+}
